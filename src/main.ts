394063cfa5a2c89bea4634154c04c12c9177d79a
Ahmed Elashmony <ahmedelashmony59@gmail.com>
2024-09-22 14:47:01 +0300

    body-parser

--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import * as dotenv from 'dotenv';
 import { AppModule } from './app.module';
-import { json } from 'body-parser';
+import { json, urlencoded } from 'body-parser';
 import { Request, Response } from 'express';
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.use(
-    '/webhook',
+    '/order/webhook',
     json({
       verify: (req: Request, res: Response, buf: Buffer) => {
         req['rawBody'] = buf;
       },
     }),
   );
+  app.use(json());
+  app.use(urlencoded({ extended: true }));
+
   app.enableCors();
   await app.listen(process.env.PORT);
 }
